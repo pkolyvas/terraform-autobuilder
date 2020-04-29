30b36d1bf8f452c5ea1285faecc2a755abd852ff
Petros Kolyvas <code@petros.io>
2020-04-29 15:43:52 -0400

    Fork & Branch & Go Version now supported

--- bionic/bootstrap.sh
+++ bionic/bootstrap.sh
@@ -1,21 +1,4 @@
 #!/usr/bin/env bash
 
-goVer=1.12.10
-
 apt-get update
 apt-get install -y build-essential
-
-echo Setting up Go...
-
-echo Downloading Go...
-wget --quiet https://dl.google.com/go/go$goVer.linux-amd64.tar.gz
-
-echo Unpacking Go...
-tar xf go$goVer.linux-amd64.tar.gz
-
-echo Cleaning up a previous go install incase of reprovisioning...
-rm -rf /usr/local/go
-
-echo putting Go in its place...
-mv -f ./go /usr/local
-
--- bionic/terraform.sh
+++ bionic/terraform.sh
@@ -1,9 +1,45 @@
 #!/usr/bin/env bash
 
-# Set your branch here
-tfBranch=$(eval echo \$\{BRANCH\})
+# Evaluate whether to build from official repo or fork
+if [ -z "$TF_FORK"]
+then
+    tfFork=hashicorp
+else
+    tfFork=$(eval echo \$\{TF_FORK\})
+fi
 
-# mkdir go
+if [ -z "$TF_BRANCH"] 
+then
+    tfBranch=master
+else
+    tfBranch=$(eval echo \$\{TF_BRANCH\})
+fi
+
+echo Cloning the Terraform repository
+git clone https://github.com/$tfFork/terraform.git src/github.com/hashicorp/terraform
+cd src/github.com/hashicorp/terraform
+
+echo Checking out the branch $tfBranch
+git checkout $tfBranch
+
+goVer="$(cat .go-version)"
+
+echo Setting up Go...
+
+echo Downloading Go...
+wget --quiet https://dl.google.com/go/go$goVer.linux-amd64.tar.gz
+
+echo Unpacking Go...
+tar xf go$goVer.linux-amd64.tar.gz
+
+echo Cleaning up a previous go install incase of reprovisioning...
+sudo rm -rf /usr/local/go
+
+echo putting Go in its place...
+sudo mv -f ./go /usr/local
+
+make tools
+
 
 echo Exporting appropriate paths
 
@@ -12,15 +48,6 @@ export GOPATH=$HOME/go
 export PATH=$PATH:/usr/local/go/bin
 echo "GOPATH=$HOME/go" >> ~/.profile
 echo "PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.profile
-
-
-echo Cloning the Terraform repository
-git clone https://github.com/hashicorp/terraform.git src/github.com/hashicorp/terraform
-cd src/github.com/hashicorp/terraform
-
-echo Checking out the branch $tfBranch
-git checkout $tfBranch
-make tools
 
 echo Compiling/installing the feature branch
 go install
